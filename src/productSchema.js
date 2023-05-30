d5f0a18ae8dfa4f9f6f7d8b5a3d8ff14e188a46d
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-30 15:17:01 +1200

    Validated Logindetails, Pet Insurance Quote and Product

--- src/productSchema.js
+++ src/productSchema.js
@@ -1,3 +1,10 @@
-const schema = null;
+const Joi = require("joi");
+
+const schema = Joi.object({
+    title: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    image: Joi.string().allow(''),
+    description: Joi.string().max(170).required()
+});
 
 module.exports = schema;
--- src/quoteSchema.js
+++ src/quoteSchema.js
@@ -1,3 +1,9 @@
-const schema = null;
+const Joi = require("joi");
+
+const schema = Joi.object({
+    planType: Joi.string().alphanum().min(3).max(30).required(),
+    petName: Joi.string().alphanum().min(3).max(30).required(),
+    expiry: Joi.date().greater('now').required()
+})
 
 module.exports = schema;
